3525ede06f198c228bac107e80102b06ba3935e9
Documenter.jl <documenter@juliadocs.github.io>
2026-01-28 05:46:00 +0000

    build based on 5200275

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PseudoStructArrays","page":"Home","title":"PseudoStructArrays","text":"Documentation for PseudoStructArrays.\n\n","category":"section"}]
+}
